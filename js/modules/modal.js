5180a5b98f92563345ed206dab5cae0a3020c761
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2023-02-11 18:54:30 -0300

    modal

---
+++ js/modules/modal.js
@@ -0,0 +1,32 @@
+export default function initModal(){
+
+
+}
+
+const abrirModal = document.querySelector('[data-modal="abrir"]');
+const fecharModal = document.querySelector('[data-modal="fechar"]');
+const modalContainer = document.querySelector('.modal-container');
+const modalSubmit = document.querySelector('.modal button[type="submit"]');
+
+function toggleModal(event){
+  event.preventDefault()
+  modalContainer.classList.toggle('ativo');
+}
+
+function cliqueForaModal(event){
+  if(event.target === this){
+    modalContainer.classList.remove('ativo');
+  }
+}
+
+function botaoSubmit(event){
+  event.preventDefault();
+}
+
+abrirModal.addEventListener('click', toggleModal);
+
+fecharModal.addEventListener('click', toggleModal);
+
+modalContainer.addEventListener('click', cliqueForaModal);
+
+modalSubmit.addEventListener('click', botaoSubmit)
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initMostrarNoticias from "./modules/noticias.js";
 import initScrollAnime from "./modules/scroll-anime.js";
+import initModal from "./modules/modal.js"
 
 initMostrarNoticias();
 initScrollAnime();
+initModal();
