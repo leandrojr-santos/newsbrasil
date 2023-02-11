98d62129201f5d9bc07dec1866e12284f9a52f82
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2023-02-11 19:11:20 -0300

    removendo-script-nao-utilizado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 import initMostrarNoticias from "./modules/noticias.js";
-import initScrollAnime from "./modules/scroll-anime.js";
 import initModal from "./modules/modal.js"
 
 initMostrarNoticias();
-initScrollAnime();
 initModal();
