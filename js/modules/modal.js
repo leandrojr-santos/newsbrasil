3b7286bf32ff96fda5fbdb6100a814438a49e383
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2023-02-11 19:05:48 -0300

    modal2

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,32 +1,30 @@
 export default function initModal(){
-
-
-}
-
-const abrirModal = document.querySelector('[data-modal="abrir"]');
-const fecharModal = document.querySelector('[data-modal="fechar"]');
-const modalContainer = document.querySelector('.modal-container');
-const modalSubmit = document.querySelector('.modal button[type="submit"]');
-
-function toggleModal(event){
-  event.preventDefault()
-  modalContainer.classList.toggle('ativo');
-}
-
-function cliqueForaModal(event){
-  if(event.target === this){
-    modalContainer.classList.remove('ativo');
+  if(abrirModal && fecharModal && modalContainer && modalSubmit){
+    const abrirModal = document.querySelector('[data-modal="abrir"]');
+    const fecharModal = document.querySelector('[data-modal="fechar"]');
+    const modalContainer = document.querySelector('.modal-container');
+    const modalSubmit = document.querySelector('.modal button[type="submit"]');
+  
+    function toggleModal(event){
+      event.preventDefault()
+      modalContainer.classList.toggle('ativo');
+    }
+  
+    function cliqueForaModal(event){
+      if(event.target === this){
+      modalContainer.classList.remove('ativo');
+      }
+    }
+  
+    function botaoSubmit(event){
+      event.preventDefault();
+    }
+  
+    abrirModal.addEventListener('click', toggleModal);
+    fecharModal.addEventListener('click', toggleModal);
+    modalContainer.addEventListener('click', cliqueForaModal);
+    modalSubmit.addEventListener('click', botaoSubmit);
   }
 }
 
-function botaoSubmit(event){
-  event.preventDefault();
-}
 
-abrirModal.addEventListener('click', toggleModal);
-
-fecharModal.addEventListener('click', toggleModal);
-
-modalContainer.addEventListener('click', cliqueForaModal);
-
-modalSubmit.addEventListener('click', botaoSubmit)
