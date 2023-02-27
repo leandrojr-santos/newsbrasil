f03a6066358310a9beb2997226d7c91042454330
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2023-02-27 20:00:27 -0300

    responsividade

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -7,18 +7,16 @@ export default function initMenuMobile(){
 
 const btnMenu = document.querySelector('[data-menu="button"]');
 const menuLista = document.querySelector('[data-menu="lista"]');
-const eventos = ['click', 'touchstart'];
+const eventos = ['click'];
 
 eventos.forEach(userEvent =>{
   btnMenu.addEventListener(userEvent, ativarMenu);
 })
 
-btnMenu.addEventListener('click', ativarMenu);
-btnMenu.addEventListener('touchstart', ativarMenu);
 
 function ativarMenu(){
-  menuLista.classList.add('active');
-  btnMenu.classList.add('active');
+  menuLista.classList.toggle('active');
+  btnMenu.classList.toggle('active');
   outsideClick(menuLista, eventos, () =>{
     menuLista.classList.remove('active');
     btnMenu.classList.remove('active');
