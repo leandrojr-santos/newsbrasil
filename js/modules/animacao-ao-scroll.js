6ceb983621e882f6b3b0f2101f93f26ff250f4a0
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2023-03-05 18:29:07 -0300

    animacao-scroll

---
+++ js/modules/animacao-ao-scroll.js
@@ -0,0 +1,33 @@
+export default function initAnimacaoAoScroll(){
+
+  
+const sections = document.querySelectorAll('[data-anime="scroll"]');
+const titulos = document.querySelectorAll('[data-anime="titulo"]');
+console.log(sections)
+const MetadeTela = window.innerHeight * 0.6;
+
+if(sections){
+  function animaScroll(){
+    sections.forEach((section, index)=>{
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionVisivel = (sectionTop - MetadeTela) < 0;
+     
+      if(sectionVisivel){
+        if(index % 2 === 0){
+          section.classList.add('right')
+        }else{
+          section.classList.add('left')
+        }
+    
+      }
+    })
+  }
+  
+  animaScroll();
+  
+  window.addEventListener('scroll', animaScroll);
+}
+
+}
+
+
--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ import initMostrarNoticias from "./modules/noticias.js";
 import initModal from "./modules/modal.js"
 import initMenuMobile from "./modules/menu-mobile.js";
 import outsideClick from "./modules/outsideclick.js";
+import initAnimacaoAoScroll from "./modules/animacao-ao-scroll.js";
 
 initMostrarNoticias();
 initModal();
 initMenuMobile();
-outsideClick();
+initAnimacaoAoScroll();
